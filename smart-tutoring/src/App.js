0d0115c4f9bba12586618a53a8a3253eb6f7a8ab
Lena Ertl <lertl@hm.edu>
2022-12-28 15:47:53 +0100

    Location_become

--- smart-tutoring/src/App.js
+++ smart-tutoring/src/App.js
@@ -5,6 +5,7 @@ import LanguagesB from './pages/Languages_become';
 // import HeaderBlack from './components/HeaderBlack';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SchoolB from './pages/School_become';
+import LocationB from './pages/Location_become';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <Route path='/' element={<Home />}/>
     <Route path='schoolBecome' element={<SchoolB />}/>
     <Route path='languagesBecome' element={<LanguagesB />}/>
+    <Route path='locationBecome' element={<LocationB />}/>
   </Routes>
   </BrowserRouter>
   );
