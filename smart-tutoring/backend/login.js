48f7a495f63a53b30d3df5a36081f6527d869ff5
Lena Ertl <lertl@hm.edu>
2023-01-13 13:23:32 +0100

    login update

--- smart-tutoring/backend/login.js
+++ smart-tutoring/backend/login.js
@@ -1,23 +1,28 @@
-const app = express ();
+import express from 'express';
+import User from './models/userStudent';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-app.use(express.json({limit: "50mb" }));
+const app = express();
 
-app.post("/register", async (req, res)=> {
-    try{
-        const { first_name, last_name, email, passwort, role} = req.body;
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/register", async (req, res) => {
+    try {
+        const { first_name, last_name, email, passwort } = req.body;
 
         if (!(email && passwort && first_name && last_name)) {
             res.status(400).send("All input is requiered");
         }
-        const oldUser = await User.findOne ({email});
+        const oldUser = await User.findOne({ email });
 
         if (oldUser) {
             return res.status(409).send("User Already Exist. Pleas Login");
         }
 
-        encryptedPassword = await bcrypt.hash (passwort, 10);
+        const encryptedPassword = await bcrypt.hash(passwort, 10);
 
-        const user = await User.create ({
+        const user = await User.create({
             first_name,
             last_name,
             email: email.toLowerCase(),
@@ -25,19 +30,16 @@ app.post("/register", async (req, res)=> {
             role: "default"
         });
 
-        /*var privateKey = fs.readFileSync ('Keys/privat.key');
+
         const token = jwt.sign(
-            {user_id: user._id, email, role:["RETRIEVE_ALL_BOOKINGS", "CONFIRM_BOOKING"],},
-            privateKey, {algorithm: 'RS256'},
-            {
-                expiresIn: "2h",
-            }
-        );*/
+            { user_id: user._id, email },
+            { expiresIn: "2h" }
+        );
 
         user.token = token;
 
         res.status(201).json(user);
-    }catch(err) {
+    } catch (err) {
         console.log(err);
     }
 });
