e952a276bf7569b96116a43f7eadef30a316dd45
Sarah <s.obermaier@hm.edu>
2023-01-15 15:19:50 +0100

    Set Time

---
+++ smart-tutoring/src/components/SetTime.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
+
+
+export default function ResponsiveTimePickers() {
+  const [value, setValue] = React.useState(dayjs('2018-01-01T00:00:00.000Z'));
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div style={{
+                    position: 'absolute',
+                    width: '315px',
+                    height: '50px',
+                    left: '34px',
+                    top: '435px',
+                }}>
+      <Stack spacing={3}>
+        <MobileTimePicker
+          label="Zeit"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        </Stack>
+        </div>
+        </LocalizationProvider>
+  )
+}
--- smart-tutoring/src/pages/SetDate.js
+++ smart-tutoring/src/pages/SetDate.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import Date from '../components/Date';
 import Stack from '@mui/material/Stack';
 import HeaderBlack from '../components/HeaderBlack';
-
+import SetTime from '../components/SetTime';
 
 const SetDate = () => {
     return <div>
         <Stack>
             <HeaderBlack />
             <Date />
+            <SetTime/>
         </Stack>
     </div>
 }
